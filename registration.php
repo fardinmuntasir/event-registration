3598f86897121a499f96ff5cb583cb11bcf55ee0
fardinmuntasir <fardinmuntasir@gmail.com>
2021-04-17 20:49:13 +0600

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,50 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+mysqli_select_db($conn, 'db_fpd');
+
+$sql = mysqli_query($conn, "SELECT * FROM tbl_countries");
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}else{
+	if($_POST)
+{
+	$first_name=$_POST['first_name'];
+	$last_name=$_POST['last_name'];
+	$gender=$_POST['gender'];
+	$age=$_POST['age'];
+	$position=$_POST['position'];
+	$company=$_POST['company'];
+	$country=$_POST['country'];
+	$code=$_POST['code'];
+	$phone=$_POST['phone'];
+	$email=$_POST['email'];
+
+	$insert = "INSERT INTO tbl_registrations
+	VALUES (NULL, '$first_name', '$last_name', '$gender', '$age', '$position', '$company', '$country', '$code', '$phone', '$email')";
+
+	if (mysqli_query($conn, $insert)) {
+		echo "New record created successfully";
+		header('Location: '. "http://" . $_SERVER['SERVER_NAME'] ."/fardin/event/index.php?mess=1");
+		exit();
+	  
+	} else {
+	  echo "Error: " . $insert . "<br>" . mysqli_error($conn);
+	}
+
+	mysqli_close($conn);
+}
+
+}
+
+
+
+
+?>
